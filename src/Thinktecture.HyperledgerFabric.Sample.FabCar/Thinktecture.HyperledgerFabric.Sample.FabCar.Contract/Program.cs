5290d2bdefd926db646066888da4d15306ef20fa
Manuel Rauber <manuel.rauber@thinktecture.com>
2018-08-29 12:25:11 +0200

    feat: fabcar contracts

--- src/Thinktecture.HyperledgerFabric.Sample.FabCar/Thinktecture.HyperledgerFabric.Sample.FabCar.Contract/Program.cs
+++ src/Thinktecture.HyperledgerFabric.Sample.FabCar/Thinktecture.HyperledgerFabric.Sample.FabCar.Contract/Program.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Thinktecture.HyperledgerFabric.Chaincode;
 using Thinktecture.HyperledgerFabric.Chaincode.Handler;
 
-namespace FabCar
+namespace Thinktecture.HyperledgerFabric.Sample.FabCar.Contract
 {
     class Program
     {
         static async Task Main(string[] args)
         {
-            using (var provider = ChaincodeProviderConfiguration.Configure<Thinktecture.HyperledgerFabric.Sample.FabCar.FabCar>(args))
+            using (var provider = ChaincodeProviderConfiguration.ConfigureWithContracts<global::Thinktecture.HyperledgerFabric.Sample.FabCar.Contract.FabCar>(args))
             {
                 var shim = provider.GetRequiredService<Shim>();
                 await shim.Start();
